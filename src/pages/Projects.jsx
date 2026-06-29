4d1cca26e51c425afde3f3aeb316488cbfae7bda
Pratik <kumar.pratik42000@gmail.com>
2026-06-29 15:46:55 +0530

    fix: update project thumbnail URL for Gen-AI Resume Builder

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -15,7 +15,7 @@ function Projects() {
     },
     {
         projectname: 'Gen-AI Resume Builder',
-        projectthumbnail: 'https://private-user-images.githubusercontent.com/77845945/614503070-1c251ba7-f2f7-43f5-8f16-39d6a13a051b.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3ODI3Mjc4MTMsIm5iZiI6MTc4MjcyNzUxMywicGF0aCI6Ii83Nzg0NTk0NS82MTQ1MDMwNzAtMWMyNTFiYTctZjJmNy00M2Y1LThmMTYtMzlkNmExM2EwNTFiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNjA2MjklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjYwNjI5VDEwMDUxM1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTQzMTFhZWE4ZTU0ZDU1ZGZkMjM2N2FkNjNiZjQ1YzIzMzAyOTk1MGQ4Zjg1NTdmMzc1ZGM5NWQzMGVhZTAxYjMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JnJlc3BvbnNlLWNvbnRlbnQtdHlwZT1pbWFnZSUyRnBuZyJ9.7Xd4sVvoAJoq49FnT-6gjZ0sXypOx-UOpS_rWMmR4gY',
+        projectthumbnail: 'https://github.com/user-attachments/assets/1c251ba7-f2f7-43f5-8f16-39d6a13a051b',
         projectdesc: 'An interview planner and resume build using Google Gemini API.',
         github: 'https://github.com/pratikkumar911/gen-ai-resume-builder',
         website: 'https://drive.google.com/file/d/17NJW1Zpn8_jA_OCfddQA6m1ZnOZxoQoi/view?usp=sharing',
