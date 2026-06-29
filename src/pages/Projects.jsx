4963ebc30844e940bca431fb93774fee0256e82e
Pratik <kumar.pratik42000@gmail.com>
2026-06-29 15:41:19 +0530

    refactor: update project list in Projects component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,47 +6,20 @@ function Projects() {
     // rely on NavLink's active state instead of manually toggling classes
     
     let projects =[{
-        projectname: 'Co-Vax',
-        projectthumbnail: 'https://user-images.githubusercontent.com/77845945/147767998-0e178565-5745-4a33-9e50-0eee53d1eadd.png',
-        projectdesc: 'Co-Vax is a website that helps people to find nearby vaccination center.',
-        github: 'https://github.com/pratikkumar911/covax',
-        website: 'https://co-vax-savelives.netlify.app/',
-        tools: ['ReactJS','CowinAPI'],
-    },{
-        projectname: 'Sigma Rules',
-        projectthumbnail: 'https://user-images.githubusercontent.com/77845945/147679076-c01f79e6-59f8-44d0-a2b4-378223135b26.png',
-        projectdesc: 'A full stack app where u can store sigma rules and watch others rules too.',
-        github: 'https://github.com/pratikkumar911/Sigma_Rules',
-        website: 'https://sigma-rule-69.netlify.app/',
-        tools: ['ReactJS', 'Tailwind CSS', 'Framer Motion'],
-    },{
         projectname: 'Chill',
         projectthumbnail: 'https://user-images.githubusercontent.com/77845945/152189229-76d96529-1593-4950-a641-7c5c76cc4f06.png',
         projectdesc: 'A music player(moodly website clone) where you can choose your own theme.',
         github: 'https://github.com/pratikkumar911/Moodly',
         website: 'https://chill-lofi.netlify.app/',
         tools: ['NextJS', 'Tailwind CSS'],
-    },{
-        projectname: 'Portfolio 1.0',
-        projectthumbnail: 'https://user-images.githubusercontent.com/77845945/147679325-b41a8378-b2c9-4032-8cd9-6c13ba1a3530.png',
-        projectdesc: 'A minimal personal website made using html, css and javascript.',
-        github: 'https://github.com/pratikkumar911/portfolio',
-        website: 'https://pratik-codes.herokuapp.com/',
-        tools: ['HTML', 'CSS', 'JS'],
-    },{
-        projectname: 'One Page',
-        projectthumbnail: 'https://user-images.githubusercontent.com/77845945/143686108-ac59b52a-9580-42d9-9f60-c72a0de115f2.png',
-        projectdesc: 'A custom startpage using React. It is not responsive though.',
-        github: 'https://github.com/pratikkumar911/one-page',
-        website: 'https://one-punch-page.herokuapp.com/',
-        tools: ['ReactJS'],
-    },{
-        projectname: 'Gradient Generator',
-        projectthumbnail: 'https://user-images.githubusercontent.com/77845945/147767947-43dc44fe-b84a-4063-bb20-e9db6a26317f.png',
-        projectdesc: 'Generates gradients of two colurs with various shades.',
-        github: 'https://github.com/pratikkumar911/gradient',
-        website: 'https://colorgradient.herokuapp.com/',
-        tools: ['ReactJS'],
+    },
+    {
+        projectname: 'Gen-AI Resume Builder',
+        projectthumbnail: 'https://private-user-images.githubusercontent.com/77845945/614503070-1c251ba7-f2f7-43f5-8f16-39d6a13a051b.png?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3ODI3Mjc4MTMsIm5iZiI6MTc4MjcyNzUxMywicGF0aCI6Ii83Nzg0NTk0NS82MTQ1MDMwNzAtMWMyNTFiYTctZjJmNy00M2Y1LThmMTYtMzlkNmExM2EwNTFiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNjA2MjklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjYwNjI5VDEwMDUxM1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTQzMTFhZWE4ZTU0ZDU1ZGZkMjM2N2FkNjNiZjQ1YzIzMzAyOTk1MGQ4Zjg1NTdmMzc1ZGM5NWQzMGVhZTAxYjMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JnJlc3BvbnNlLWNvbnRlbnQtdHlwZT1pbWFnZSUyRnBuZyJ9.7Xd4sVvoAJoq49FnT-6gjZ0sXypOx-UOpS_rWMmR4gY',
+        projectdesc: 'An interview planner and resume build using Google Gemini API.',
+        github: 'https://github.com/pratikkumar911/gen-ai-resume-builder',
+        website: 'https://drive.google.com/file/d/17NJW1Zpn8_jA_OCfddQA6m1ZnOZxoQoi/view?usp=sharing',
+        tools: ['ReactJS','ExpressJS','Gemini API'],
     }]
     return (
         <div className="projects">
